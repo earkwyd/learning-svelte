4753dbf2e1ed5ac9a81aa1518553ee808d5c2c0d
Thiago Marchesini <thiago_marchesini@hotmail.com>
2023-09-20 17:56:33 -0300

    ✨ Feat: created coffee score card component

---
+++ src/routes/CoffeeScoreCard.svelte
@@ -0,0 +1,20 @@
+<script>
+  import imagePath from "$lib/images/mulher.png";
+</script>
+
+<div class="bg-gray-200 w-full shadow">
+  <div class="flex items-center pl-2 justify-between">
+    <div class="flex flex-row">
+      <img
+        src={imagePath}
+        class="w-10 h-10 rounded-full self-center"
+        alt="foto da mulher"
+      />
+      <div class="py-3 px-3">
+        <div class="font-semibold text-sm">Sr.Caique</div>
+        <div class="text-xs text-gray-500">Vivenda Comissária Café</div>
+      </div>
+    </div>
+    <div class="text-xs text-gray-500 pr-2">sáb, 5 de fev. de 2022</div>
+  </div>
+</div>
